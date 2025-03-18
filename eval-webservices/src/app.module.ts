0d0ef96c24da1cd8a805422c4e1dc91b090cd7cc
LordHugo02 <hugoflinois@hotmail.fr>
2025-03-18 11:38:22 +0100

    update app module

--- eval-webservices/src/app.module.ts
+++ eval-webservices/src/app.module.ts
@@ -5,6 +5,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
+import { UserResolver } from './graphql/resolvers/user.resolver';
+import { RoomResolver } from './graphql/resolvers/room.resolver';
+import { ReservationResolver } from './graphql/resolvers/reservation.resolver';
+import { UserEntity } from './entities/user.entity';
+import { RoomEntity } from './entities/room.entity';
+import { ReservationsEntity } from './entities/reservation.entity';
 
 @Module({
   imports: [
@@ -13,18 +19,18 @@ import { join } from 'path';
       host: 'localhost',
       port: 5432,
       username: 'pguser',
-      password: 'pgpassword',
-      database: 'votre_database',
-      entities: [], //mettre les entities
-      synchronize: false, // false si vous avez déjà les tables
+      password: 'pgpass',
+      database: 'pgdb',
+      entities: [UserEntity, RoomEntity, ReservationsEntity], //mettre les entities
+      synchronize: true, // false si vous avez déjà les tables
     }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
       driver: ApolloDriver,
     }),
-    TypeOrmModule.forFeature([]), //mettre les entities
+    TypeOrmModule.forFeature([UserEntity, RoomEntity, ReservationsEntity]), //mettre les entities
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, UserResolver, RoomResolver, ReservationResolver],
 })
 export class AppModule {}
