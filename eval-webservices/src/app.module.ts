fb44a5fead3ca59250c2e09ca9c2fe9448b0f75d
LordHugo02 <hugoflinois@hotmail.fr>
2025-03-18 09:57:08 +0100

    update app module for graphql

--- eval-webservices/src/app.module.ts
+++ eval-webservices/src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'pguser',
+      password: 'pgpassword',
+      database: 'votre_database',
+      entities: [], //mettre les entities
+      synchronize: false, // false si vous avez déjà les tables
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      driver: ApolloDriver,
+    }),
+    TypeOrmModule.forFeature([]), //mettre les entities
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
