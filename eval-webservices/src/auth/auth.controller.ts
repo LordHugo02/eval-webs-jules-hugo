2a83925907f29895aaf2bb36d15cfcce58223c8f
Blewkse <julesbloux@gmail.com>
2025-05-19 10:18:43 +0200

    fix: nest controller does not handle response when res is specified un parameters

--- eval-webservices/src/auth/auth.controller.ts
+++ eval-webservices/src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Req, Res } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common';
 import { Request, Response } from 'express';
 import { AuthService } from './auth.service';
 
@@ -11,19 +11,15 @@ type UserLoginPayload = {
 export class AuthController {
   constructor(private authService: AuthService) {}
 
-  @Get('loginByClientPortal')
-  loginByClientPortal(
-    userLoginPayload: UserLoginPayload,
-    @Req() req: Request,
-    @Res() res: Response,
-  ) {
+  @Post('loginByClientPortal')
+  loginByClientPortal(@Req() req: Request, @Res() res: Response) {
     console.log('login');
-    this.authService.loginByClientPortal(res);
+    return this.authService.loginByClientPortal(res);
   }
-  @Get('login')
-  login(userLoginPayload: UserLoginPayload) {
-    console.log('login');
-    this.authService.login(userLoginPayload);
+  @Post('login')
+  async login(@Body() payload: UserLoginPayload) {
+    const token = await this.authService.login(payload);
+    return token; // Nest gère le res.status(200).json(token)
   }
 
   @Get('callback')
@@ -34,6 +30,6 @@ export class AuthController {
 
   @Get('logout')
   logout(@Req() req: Request, @Res() res: Response) {
-    this.authService.logout(req, res);
+    return this.authService.logout(req, res);
   }
 }
